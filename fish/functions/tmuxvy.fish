010c74e4811ac7cf669dea70475d8f1b12af92c1
Vadim Brodsky <vadim.brodsky@gmail.com>
2017-10-27 10:43:03 -0400

    Update functions

--- fish/functions/tmuxvy.fish
+++ fish/functions/tmuxvy.fish
@@ -7,6 +7,7 @@ function tmuxvy
   tmux select-pane -t 1
   tmux send-keys -t vy 'cd /vidyard/DevTools' C-m
   tmux send-keys -t vy 'git pull' C-m
+  tmux send-keys -t vy './start.dev'
 
   tmux split-window -v -t vy
   tmux select-pane -t 1
@@ -14,16 +15,19 @@ function tmuxvy
 
   tmux send-keys -t vy 'cd /vidyard/VidyardDashboard' C-m
   tmux send-keys -t vy 'git pull' C-m
+  tmux send-keys -t vy './start.image'
 
   tmux select-pane -t 3
   tmux send-keys -t vy 'cd /vidyard/Multipass' C-m
   tmux send-keys -t vy 'git pull' C-m
+  tmux send-keys -t vy './start.u'
 
   tmux select-pane -t 4
   tmux send-keys -t vy 'ctop' C-m
   tmux split-window -v -t vy
 
   tmux send-keys -t vy 'cd /vidyard/Guardtower' C-m
+  tmux send-keys -t vy './start.dev'
 
   tmux new-window -n editor -t vy
   tmux send-keys -t vy 'cd /vidyard' C-m

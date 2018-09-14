6dbcbb093b3941299006837600c808a71ecaa560
Vadim Brodsky <vadim.brodsky@gmail.com>
2018-09-14 16:31:13 -0400

    Add dashboard migration

--- fish/functions/dashmig.fish
+++ fish/functions/dashmig.fish
@@ -1,6 +1,6 @@
 function dashmig
   cd /vidyard/VidyardDashboard/
-  docker-compose run --rm web bin/rake db:migrate
+  docker-compose -f docker-compose.images.yml run --rm web bin/rake db:migrate
   cd -
 end
 

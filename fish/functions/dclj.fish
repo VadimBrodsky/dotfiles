4e10c4d86f54b305d07fa083c97e9202aeba81a2
Vadim Brodsky <vadim.brodsky@gmail.com>
2019-05-26 22:43:55 -0400

    Expose port 3000 in the docker clojure env

--- fish/functions/dclj.fish
+++ fish/functions/dclj.fish
@@ -1,3 +1,3 @@
 function dclj
-  docker run -it --rm --name clojure -e USER=$USER -v "$PWD:/usr/src/" -w /usr/src/ clojure:openjdk-8-lein-alpine sh
+  docker run -it --rm --name clojure -e USER=$USER -p 3000:3000 -v "$PWD:/usr/src/" -w /usr/src/ clojure:openjdk-8-lein-alpine sh
 end

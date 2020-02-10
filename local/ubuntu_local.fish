a1f9297bab0e2f4ec37372431b082df5de8a7577
Vadim Brodsky <vadim.brodsky@gmail.com>
2020-02-09 22:56:22 -0500

    Add fix for ssh-agent issues in fish

--- local/ubuntu_local.fish
+++ local/ubuntu_local.fish
@@ -3,3 +3,9 @@ set -xg PATH /snap/bin $PATH
 
 # aliases
 alias kubectl='microk8s.kubectl'
+
+# Fix for Ssh agent issues
+# eval (ssh-agent -c)
+# set -Ux SSH_AUTH_SOCK $SSH_AUTH_SOCK
+# set -Ux SSH_AGENT_PID $SSH_AGENT_PID
+# set -Ux SSH_AUTH_SOCK $SSH_AUTH_SOCK

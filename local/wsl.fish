4b08522de60ab178df76fef69026c7f92011c311
Vadim Brodsky <vadim.brodsky@gmail.com>
2020-03-28 12:56:21 -0400

    Add WSL specific Fish overrides

---
+++ local/wsl.fish
@@ -0,0 +1,9 @@
+# aliases
+alias docker='docker.exe'
+alias docker-compose='docker-compose.exe'
+
+# Fix for Ssh agent issues
+# eval (ssh-agent -c)
+# set -Ux SSH_AUTH_SOCK $SSH_AUTH_SOCK
+# set -Ux SSH_AGENT_PID $SSH_AGENT_PID
+# set -Ux SSH_AUTH_SOCK $SSH_AUTH_SOCK

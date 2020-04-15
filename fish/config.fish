b3e3625f85599675cc1efa56b7fb3de08b6ffca3
Vadim Brodsky <vadim.brodsky@gmail.com>
2020-04-15 09:14:25 -0400

    Cleanup fish config in favour of asdf

--- fish/config.fish
+++ fish/config.fish
@@ -1,31 +1,5 @@
-# install fundle
-# wget https://git.io/fundle -O ~/.config/fish/functions/fundle.fish
-#
-# update fundle
-# fundle self-update
-
-## Fundle plugins, run fundle install to install them
-# fundle plugin 'edc/bass'
-# fundle init
-
-# ruby
-set PATH /usr/local/bin $PATH
-set PATH $HOME/.rbenv/shims $PATH
-rbenv rehash >/dev/null ^&1
-
-# node
-# https://github.com/brigand/fast-nvm-fish
-set -U fish_user_paths
-nvm use 8.16.0
-
-# python 2 for mac
-export PATH="/usr/local/opt/python/libexec/bin:$PATH"
-
-# rust
-export PATH="/Users/vbrodsky/.cargo/bin:$PATH"
-
-# yarn
-set -xg PATH usr/local/bin/yarn $PATH 
+# asdf
+source ~/.asdf/asdf.fish
 
 # editor
 set EDITOR nvim
@@ -44,5 +18,5 @@ alias la='ls -A'
 alias lh='ls -lah'
 alias l='ls -CF'
 alias g=git
-alias gg='git grep'
+alias gg='git grep -n'
 alias vim=nvim
